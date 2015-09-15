c33d5be9edb79aaa19ab38c3ff8cb4299f5cfef5
Awah Ndingwan <Awahndin@udel.edu>
2015-09-15 19:54:41 -0400

    Upload

---
+++ src/MainPackage/MyInteger.java
@@ -0,0 +1,39 @@
+package MainPackage;
+
+public class MyInteger {
+	int value;
+	
+	public static void main (String[] args)  {
+		
+		System.out.println("My Number is: " + MyInteger());
+	}
+
+	public static int MyInteger() {
+		
+		int value = 1;
+		return (value);
+	}
+	public static void isEven() {
+		
+	}
+	public static void isOdd() {
+		
+	}
+	public static void isPrime() {
+		
+		}
+	public static int isEven(int x) {
+		x = 1; //This is suppose to be an if/else
+		//it will accept an integer x and return a
+		// statement if its true or false
+		return (x);
+		}
+	public static int isOdd(int x) {
+		x = 1;
+		return (x);
+	}
+	public static int isPrime(int x) {
+		x = 1;
+		return (x);
+	}
+}
