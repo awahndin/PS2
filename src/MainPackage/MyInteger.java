62971a7576ee682680a5e0b3dc492e1007c3d742
Awah Ndingwan <Awahndin@udel.edu>
2015-09-15 22:40:26 -0400

    commit

--- src/MainPackage/MyInteger.java
+++ src/MainPackage/MyInteger.java
@@ -4,7 +4,7 @@ public class MyInteger {
 	int value = 1;
 	
 	public static void main (String[] args)  {
-		System.out.println("My Number is: " );
+		System.out.println("My Number is: ");
 		System.out.println("this will be if its even or odd(0 is even)");
 		System.out.println("this will be if its prime or not");
 	}
@@ -12,8 +12,13 @@ public class MyInteger {
 	public MyInteger() {
 		int value = 1;
 	}
+	
+	public int getValue() {
+		
+		return value;
+	}
 	public  boolean isEven() {
-		//this should have the if/else statements
+		
 		if (value %2 == 0)
 			return true;
 		else {
@@ -32,14 +37,35 @@ public class MyInteger {
 		if (value %2 != 0)
 			return true;
 		else {
-			
+			return false;
 		}
-			
-		
 	}
-	public void isPrime() {
-		
+	public static boolean isOdd(int num) {
+		if (num %2 != 0)
+			return true;
+		else {
+			return false;
+				}	
+	}
+	public boolean isPrime() {
+		int i;
+		for (i =2; i < value; i++) {
+			int n = value % i;
+		if (n == 0) {
+			return true;
 		}
-	
+		}
+		return false;
+	}
+	public static boolean isPrime(int num) {
+		int i;
+		for (i = 2; i < num; i++) {
+			int n = num % i;
+		if (n == 0) {
+			return true;
+		}
+		}
+		return false;
+	}
 		
 }
